91ce69cb763251ce659058229f5a211024988818
Anatolii <Anatolii-med@users.noreply.github.com>
2022-02-15 00:06:41 +0200

    render form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,37 @@
 import React from 'react';
+import Section from './section/section';
 
-class App extends React.Component() {
+class App extends React.Component {
     state = {
         contacts: [],
         name: '',
     };
 
-    render() {}
+    render() {
+        return (
+            <>
+                <Section title="Phonebook">
+                    <form>
+                        <label htmlFor="name">
+                            <input
+                                type="text"
+                                name="name"
+                                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                                required
+                            />
+                        </label>
+                        <button type="submit">Add contact</button>
+                    </form>
+                </Section>
+                <Section title="Contacts">
+                    <ul>
+                        <li></li>
+                    </ul>
+                </Section>
+            </>
+        );
+    }
 }
 
 export default App;
